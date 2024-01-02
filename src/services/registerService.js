61c9dedfa3a1bfd079f0dfa54f3f4e3281fcb8e5
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2024-01-02 14:46:11 -0300

    correção service

--- src/services/registerService.js
+++ src/services/registerService.js
@@ -2,17 +2,18 @@
 import { api, requestConfig } from "../utils/config";
 
 const register = async (data) => {
-    const config = requestConfig("POST", data);;
-  
-    try {
-      const res = await fetch(api + "/users/register", config)
-        .then((res) => res.json())
-        .catch((err) => err);
-      return res;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const config = requestConfig("POST", data);
+
+  try {
+      const res = await fetch(api + "/users/register", config);
+      const jsonData = await res.json();
+      console.log("Resposta da API:", jsonData); // Registre os dados da resposta
+      return jsonData;
+  } catch (error) {
+      console.error("Erro na API:", error);
+      throw error; // Rejeite o erro para ser capturado na função de chamada
+  }
+};
 
   const registerService = {
     register,
